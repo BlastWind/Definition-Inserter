6ecf60de6f98a4cf23649d85cf659ecd1a6a0395
Flober <andrewchen14250@gmail.com>
2021-04-11 02:28:11 -0400

    if not even defined, don't save to study list nonono!!!

--- background.js
+++ background.js
@@ -1,54 +1,56 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-	const { word, messageType } = request;
-	console.log({ messageType });
+  const { word, messageType } = request;
+  console.log({ messageType });
 
-	switch (messageType) {
-		case "save-to-study-list":
-			saveToStudyList(word)
-				.then((definition) => {
-					sendResponse(definition);
-				})
-				.catch((err) => {
-					console.log(err);
-				});
-		case "insert-definition":
-			getDefinition(word)
-				.then((definition) => {
-					sendResponse(definition);
-				})
-				.catch((err) => {
-					console.log(err);
-				});
-		default:
-			break;
-	}
+  switch (messageType) {
+    case "save-to-study-list":
+      saveToStudyList(word)
+        .then((definition) => {
+          sendResponse(definition);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    case "insert-definition":
+      getDefinition(word)
+        .then((definition) => {
+          sendResponse(definition);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    default:
+      break;
+  }
 
-	return true;
+  return true;
 });
 
 function saveToStudyList(word) {
-	return new Promise(function (resolve, reject) {
-		// fetch wikipedia with ${word} as parameter to set var definition
-		getDefinition(word).then((definition) => {
-			var toStore = {};
-			toStore[word] = definition;
-			chrome.storage.sync.set(toStore, function () {
-				if (chrome.runtime.error) {
-					reject("error during chrome storage set");
-				} else {
-					chrome.storage.sync.get(word, function (definition) {
-						//let's check if it is indeed saved
-						if (chrome.runtime.error) {
-							reject("error during chrome storage get");
-						} else {
-							console.log("we are at returning place!");
-							resolve(definition);
-						}
-					});
-				}
-			});
-		});
-	});
+  return new Promise(function (resolve, reject) {
+    // fetch wikipedia with ${word} as parameter to set var definition
+    getDefinition(word).then((definition) => {
+      if (definition) {
+        var toStore = {};
+        toStore[word] = definition;
+        chrome.storage.sync.set(toStore, function () {
+          if (chrome.runtime.error) {
+            reject("error during chrome storage set");
+          } else {
+            chrome.storage.sync.get(word, function (definition) {
+              //let's check if it is indeed saved
+              if (chrome.runtime.error) {
+                reject("error during chrome storage get");
+              } else {
+                console.log("we are at returning place!");
+                resolve(definition);
+              }
+            });
+          }
+        });
+      }
+    });
+  });
 }
 
 /**
@@ -57,30 +59,33 @@ function saveToStudyList(word) {
  * @returns {String} The definition or description of given term.
  */
 async function getDefinition(term) {
-	// Check wikipedia
-	let title = term.replace(" ", "_");
-	let response = await fetch(
-		`https://en.wikipedia.org/w/api.php?action=query&redirects&prop=pageprops&format=json&titles=${title}`,
-	);
-	let data = await response.json();
-	let pages = data?.["query"]?.["pages"];
-	let shortDesc =
-		pages[Object?.keys(pages)?.[0]]?.["pageprops"]?.["wikibase-shortdesc"];
-	// Check if Wikipedia has valid descriptor
-	if (
-		!shortDesc ||
-		shortDesc ===
-			"Disambiguation page providing links to topics that could be referred to by the same search term"
-	) {
-		//TODO - Check Dictionary.com, handle more than 1 definition
-		response = await fetch(
-			`https://api.dictionaryapi.dev/api/v2/entries/en_US/${term}`,
-		);
-		data = await response.json();
-		if (!Array.isArray(data)) {
-			return "";
-		}
-		shortDesc = data[0]["meanings"][0]["definitions"][0]["definition"].slice(0, -1);
-	}
-	return shortDesc;
+  // Check wikipedia
+  let title = term.replace(" ", "_");
+  let response = await fetch(
+    `https://en.wikipedia.org/w/api.php?action=query&redirects&prop=pageprops&format=json&titles=${title}`
+  );
+  let data = await response.json();
+  let pages = data?.["query"]?.["pages"];
+  let shortDesc =
+    pages[Object?.keys(pages)?.[0]]?.["pageprops"]?.["wikibase-shortdesc"];
+  // Check if Wikipedia has valid descriptor
+  if (
+    !shortDesc ||
+    shortDesc ===
+      "Disambiguation page providing links to topics that could be referred to by the same search term"
+  ) {
+    //TODO - Check Dictionary.com, handle more than 1 definition
+    response = await fetch(
+      `https://api.dictionaryapi.dev/api/v2/entries/en_US/${term}`
+    );
+    data = await response.json();
+    if (!Array.isArray(data)) {
+      return "";
+    }
+    shortDesc = data[0]["meanings"][0]["definitions"][0]["definition"].slice(
+      0,
+      -1
+    );
+  }
+  return shortDesc;
 }
